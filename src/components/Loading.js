ce5a6b25544a051a3f9a61a1f1376b4f20223063
liamcharger <chargerelectronics19@gmail.com>
2024-07-27 09:16:30 -0400

    Loading: add progress indicator

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,10 @@
 import styles from '../styles/Loading.module.css';
+import { OrbitProgress } from 'react-loading-indicators';
 
 function Loading() {
     return (
         <div className={styles.loadingCtn}>
+        <OrbitProgress dense color="#e3e3e3" size="medium" text="." textColor="" />
         <div className={styles.txtCtn}>
         <h2>Loading</h2>
         <div className={styles.cpt}>
