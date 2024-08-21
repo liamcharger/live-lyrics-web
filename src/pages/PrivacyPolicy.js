875906b7cbb5fc92e8278fe862f48f6ffe5631b4
liamcharger <chargerelectronics19@gmail.com>
2024-08-20 22:54:13 -0400

    UI: style privacy policy

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,34 +1,36 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
-import Navbar from "../components/Navbar";
+// import { useLocation } from "react-router-dom";
+// import Navbar from "../components/Navbar";
+import logo from "../assets/logo-dark.png";
 import styles from "../styles/PrivacyPolicy.module.css";
 
 function PrivacyPolicy() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const showNav = queryParams.get("navbar") === "true";
+  // const location = useLocation();
+  // const queryParams = new URLSearchParams(location.search);
+  // const showNav = queryParams.get("inApp") === "false";
 
   return (
     <>
-      {showNav ? <Navbar /> : null}
+      {/* {showNav ? <Navbar /> : null} */}
       <div className={styles.ctn}>
-        <h1>Privacy Policy</h1>
+        <div className={styles.header}>
+        <img src={logo} alt="Logo"></img>
+        <h1>Live Lyrics{'\n'}Privacy Policy</h1>
         <p>
           Live Lyrics is committed to protecting the privacy and security of our
           users. This Privacy Policy outlines how we collect, use, store, and
           disclose information when you use our Live Lyrics mobile application
           (the "App").
         </p>
-
+        </div>
+        <div className={styles.divider}></div>
         <h2>Information Collection and Use</h2>
-
         <h3>Personal Information:</h3>
         <p>
           We only collect the necessary personally identifiable information
           (PII) when you use the App, such as when you are creating an account
           or logging in.
         </p>
-
         <h3>Usage Information:</h3>
         <p>
           We may collect certain non-personal information about your use of the
@@ -36,10 +38,8 @@ function PrivacyPolicy() {
           diagnostics. This information is used to analyze and improve the
           performance and functionality of the App.
         </p>
-
         <h3>Cookies and Similar Technologies:</h3>
         <p>The App does not use cookies or similar tracking technologies.</p>
-
         <h2>Data Security:</h2>
         <p>
           We take appropriate measures to protect the information collected
@@ -48,7 +48,6 @@ function PrivacyPolicy() {
           the internet or electronic storage is completely secure, and we cannot
           guarantee absolute security.
         </p>
-
         <h2>Changes to this Privacy Policy:</h2>
         <p>
           We reserve the right to update or modify this Privacy Policy at any
@@ -56,8 +55,7 @@ function PrivacyPolicy() {
           updated Privacy Policy within the App. We encourage you to review this
           Privacy Policy periodically for any updates.
         </p>
-
-        <h2>Contact Us:</h2>
+        <div className={styles.divider}></div>
         <p>
           If you have any questions or concerns about this Privacy Policy or our
           data practices, please contact us at{" "}
@@ -66,7 +64,6 @@ function PrivacyPolicy() {
           </a>
           .
         </p>
-
         <p>
           By using Live Lyrics, you agree to the terms and conditions of this
           Privacy Policy.
